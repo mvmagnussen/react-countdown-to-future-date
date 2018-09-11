3ef3de25b441f29bbefe3c75223eaf926b4f4481
mvmagnussen <magnus.magnussen@ciber.no>
2018-09-11 22:41:08 +0200

    changed to norwegian

--- react-countdown-to-future-date.jsx
+++ react-countdown-to-future-date.jsx
@@ -1,46 +1,56 @@
-var React   = require('react');
+var React = require("react");
 var App = React.createClass({
-
-displayNone: function(e){
-    document.getElementById('PopUp').style.display = "none";
+  displayNone: function(e) {
+    document.getElementById("PopUp").style.display = "none";
     this.props.afterTimeOut();
   },
 
-  getInitialState: function(){
-    return {time: new Date(this.props.givenDate)/1000}
+  getInitialState: function() {
+    return { time: new Date(this.props.givenDate) / 1000 };
   },
 
-  update: function(){
+  update: function() {
     var newTime = this.state.time - 1; // minus one sec from initial time
-    this.setState({time:newTime});
-    if(this.state.time ===0){
+    this.setState({ time: newTime });
+    if (this.state.time === 0) {
       clearInterval(this.decr);
-      this.props.afterUnmount
+      this.props.afterUnmount;
     }
   },
 
-  render: function(){
+  render: function() {
+    var formattedGivenDate = new Date(this.props.givenDate);
+    var today = new Date();
+    var msDiff = formattedGivenDate - today;
+    var days = parseInt(msDiff / (24 * 3600 * 1000));
+    var hours = parseInt(msDiff / (3600 * 1000) - days * 24);
+    var mins = parseInt(msDiff / (60 * 1000) - days * 24 * 60 - hours * 60);
+    var secs = parseInt(
+      msDiff / 1000 - mins * 60 - days * 24 * 60 * 60 - hours * 60 * 60
+    );
 
-  var formattedGivenDate = new Date(this.props.givenDate);
-  var today = new Date();
-  var msDiff =formattedGivenDate - today;
-  var days = parseInt(msDiff/(24*3600*1000));
-  var hours =parseInt(msDiff/(3600*1000)-(days*24));
-  var mins = parseInt(msDiff/(60*1000)-(days*24*60)-(hours*60));
-  var secs = parseInt(msDiff/(1000)-(mins*60)-(days*24*60*60)-(hours*60*60));
-
-    return(
+    return (
       <div className="react-countdown-container">
-        <h1>{days, " days ", hours," hours ", mins," mins ",  secs, " secs "}</h1>
+        <span>
+          {
+            (days,
+            days === 1 ? "dag, " : " dager, ",
+            hours,
+            hours === 1 ? " time og " : " timer og ",
+            mins,
+            mins === 1 ? " minutt " : " minutter ",
+            "igjen til den store dagen")
+          }
+        </span>
       </div>
-    )
+    );
   },
-  componentDidMount: function(){
-    this.decr = setInterval(this.update,1000)
+  componentDidMount: function() {
+    this.decr = setInterval(this.update, 1000);
   },
-    
-  componentWillUnmount: function(){
+
+  componentWillUnmount: function() {
     clearInterval(this.decr);
   }
-})
+});
 module.exports = App;
